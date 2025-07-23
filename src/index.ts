c0b29e94eccea7fa4b060419eb5b8562b86831b4
Anil <d.anilchandravarma@gmail.com>
2025-07-23 16:17:24 +0530

    git changes

--- src/index.ts
+++ src/index.ts
@@ -14,6 +14,15 @@ export * from './zod/zod_utils';
 export * from './zod/zod_base_schema';
 
 // ──────── Services ────────────
+
+// ──────── Master ────────────
+
+// Device
 export * from './services/master/device/master_device_manufacturer_service';
 export * from './services/master/device/master_device_model_service';
 export * from './services/master/device/master_device_type_service';
+
+// Expense
+export * from './services/master/expense/master_expense_name_service';
+export * from './services/master/expense/master_expense_type_service';
+export * from './services/master/expense/master_vendor_type_service';
